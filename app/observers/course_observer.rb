7151c0a68e54f6a4468757a09d021cb154da8147
Mariana Lima <mlsb@cin.ufpe.br>
2018-11-29 16:54:38 -0300

    Alteração no course_observer.rb relacionado ao Plano

--- app/observers/course_observer.rb
+++ app/observers/course_observer.rb
@@ -3,10 +3,4 @@ class CourseObserver < ActiveRecord::Observer
   def after_create(course)
     Log.setup(course, :action => :create)
   end
-
-  def before_destroy(course)
-    if plan = course.plan
-      plan.audit_billable!
-    end
-  end
 end
