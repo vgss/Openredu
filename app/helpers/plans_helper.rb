afaad988892594616f75e9a752fab132b07fbcea
Mariana Lima <mlsb@cin.ufpe.br>
2018-11-30 17:13:32 -0300

    Remove o helper e o factory de plan

--- app/helpers/plans_helper.rb
+++
@@ -1,7 +0,0 @@
-# -*- encoding : utf-8 -*-
-module PlansHelper
-  # Converte de bytes para megabytes.
-  def bytes_to_mb(bytes)
-    number_with_precision((bytes / 1.megabyte.to_f), :precision => 2)
-  end
-end
