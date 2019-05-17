a04a25036e0dc7f7cfe87f4ef075c6e9044e178f
Ricardo Fagundes <rlf4@cin.ufpe.br>
2019-05-17 09:42:32 -0300

    Corrige método create em MyFileService

--- app/services/myfile_service.rb
+++ app/services/myfile_service.rb
@@ -3,9 +3,4 @@ class MyfileService < StoredContentService
   def initialize(options={})
     super options.merge(:model_class => Myfile)
   end
-
-  # Sobrescreve create por causa de autorização específica.
-  def create(&block)
-    model
-  end
 end
